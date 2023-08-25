b233ec9bfac71afea18567ce233aecbc659554a1
Michael Pishchagin <mblsha+gh@gmail.com>
2023-08-25 10:21:05 +1000

    add add_region to reduce number of read_prom calls

--- myfunc.c
+++ myfunc.c
@@ -2,46 +2,87 @@
 #include "m68k.h"
 
 #include <unordered_set>
+#include <optional>
 
-#if defined(__cplusplus)
 extern "C" {
-#endif
 typedef int (*read_mem_t)(unsigned int address, int size);
 typedef void (*write_mem_t)(unsigned int address, int size, unsigned int value);
 typedef int (*pc_hook_t)(unsigned int pc);
-#if defined(__cplusplus)
 } // extern "C"
-#endif
 
 static read_mem_t _read_mem = 0;
 static write_mem_t _write_mem = 0;
 static pc_hook_t _pc_hook = 0;
 static std::unordered_set<unsigned int> _pc_hook_addrs;
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-void set_read_mem_func(read_mem_t func) {
-  printf("set_read_mem_func: %p\n", (void*)func);
-   _read_mem = func;
-   }
-void set_write_mem_func(write_mem_t func) {
-  printf("set_write_mem_func: %p\n", (void*)func);
-  _write_mem = func; }
-void set_pc_hook_func(pc_hook_t func) {
-  printf("set_pc_hook_func: %p\n", (void*)func);
-  _pc_hook = func; }
-void add_pc_hook_addr(unsigned int addr) {
-  printf("add_pc_hook_addr: %p\n", (void*)addr);
-  _pc_hook_addrs.insert(addr);
-}
-#if defined(__cplusplus)
-} // extern "C"
-#endif
+struct Region {
+  unsigned int start_;
+  unsigned int size_;
+  uint8_t* data_;
 
-unsigned int m68k_read_memory_8(unsigned int address) { return _read_mem(address, 1); }
-unsigned int m68k_read_memory_16(unsigned int address) { return _read_mem(address, 2); }
-unsigned int m68k_read_memory_32(unsigned int address) { return _read_mem(address, 4); }
+  Region(unsigned int start, unsigned int size, void* data)
+    : start_(start), size_(size), data_(static_cast<uint8_t*>(data))
+  {}
+  ~Region() {
+    delete data_;
+  }
+
+  std::optional<unsigned int> read(unsigned int addr, int size) {
+    if (addr >= start_ && addr < start_ + size_) {
+      if (size == 1) {
+        return data_[addr - start_];
+      } else if (size == 2) {
+        return (data_[addr - start_ + 0] << 8) | data_[addr - start_ + 1];
+      } else if (size == 4) {
+        return (
+          (data_[addr - start_ + 0] << 24) |
+          (data_[addr - start_ + 1] << 16) |
+          (data_[addr - start_ + 2] << 8) |
+          data_[addr - start_ + 3]
+        );
+      }
+    }
+    return std::nullopt;
+  }
+};
+static std::vector<Region> _regions;
+
+extern "C" {
+  void set_read_mem_func(read_mem_t func) {
+    printf("set_read_mem_func: %p\n", (void*)func);
+     _read_mem = func;
+  }
+  void set_write_mem_func(write_mem_t func) {
+    printf("set_write_mem_func: %p\n", (void*)func);
+    _write_mem = func;
+  }
+  void set_pc_hook_func(pc_hook_t func) {
+    printf("set_pc_hook_func: %p\n", (void*)func);
+    _pc_hook = func;
+  }
+  void add_pc_hook_addr(unsigned int addr) {
+    printf("add_pc_hook_addr: %p\n", (void*)addr);
+    _pc_hook_addrs.insert(addr);
+  }
+  void add_region(unsigned int start, unsigned int size, void* data) {
+    printf("add_region: %p %p %p\n", (void*)start, (void*)size, data);
+    _regions.emplace_back(start, size, data);
+  }
+} // extern "C"
+
+unsigned int my_read_memory(unsigned int address, int size) {
+  for (auto& region : _regions) {
+    const auto val = region.read(address, size);
+    if (val) {
+      return *val;
+    }
+  }
+  return _read_mem(address, size);
+}
+
+unsigned int m68k_read_memory_8(unsigned int address) { return my_read_memory(address, 1); }
+unsigned int m68k_read_memory_16(unsigned int address) { return my_read_memory(address, 2); }
+unsigned int m68k_read_memory_32(unsigned int address) { return my_read_memory(address, 4); }
 
 void m68k_write_memory_8(unsigned int address, unsigned int value) { _write_mem(address, 1, value); }
 void m68k_write_memory_16(unsigned int address, unsigned int value) { _write_mem(address, 2, value); }
