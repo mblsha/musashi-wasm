b3a5e4023b3d3142c80cf9f0f2fdcb335a613e91
Michael Pishchagin <mblsha+gh@gmail.com>
2025-09-24 10:16:28 +1000

    Keep core wrapper artifacts build-only

--- npm-package/lib/wasm/musashi-node-wrapper.mjs
+++
@@ -1,6 +0,0 @@
-// ESM wrapper for the Musashi WASM module
-// This file loads the actual ESM module that should be copied here by the CI
-
-import createMusashi from './musashi-node.out.mjs';
-
-export default createMusashi;
